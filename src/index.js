7a6daa085f57d7c3b3c2538cb10f9a51710a3027
crcs-x <crisman472@hotmail.com>
2026-04-23 03:59:46 -0500

    git ad

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ let url = "https://castro.dev/";
 url = "Esto no se puede hacer";
 console.log(url);
 console.log(unused_var);
-console.log("hello git 1");
+console.log("hola fracasados de un refeneadp");
